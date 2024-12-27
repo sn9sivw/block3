e06e2a8874a1cb721f427589fc0385afca17dba6
sn9sivw <ilamokryakov@gmail.com>
2024-12-27 08:21:52 +0000

    Сделал

---
+++ task24.py
@@ -0,0 +1,13 @@
+# Перебор всех возможных значений X, Y и Z
+for X in [False, True]:
+    for Y in [False, True]:
+        for Z in [False, True]:
+            expr_a = not (Y or (not X and Z)) or Z
+            expr_b = X and not (not Y or Z) or Y
+            expr_c = not (X or (Y and Z)) or not X
+            print(f'X={X}, Y={Y}, Z={Z}:')
+            print(f'а) {expr_a}')
+            print(f'б) {expr_b}')
+            print(f'в) {expr_c}')
+            print()
+            
