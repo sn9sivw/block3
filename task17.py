076d9298969b6698ed4e46a3956ced13a2ce1e8b
sn9sivw <ilamokryakov@gmail.com>
2024-12-27 07:15:12 +0000

    Сделал

--- task17.py
+++ task17.py
@@ -1,17 +1,17 @@
 num = [(True, True), (True, False), (False, True), (False, False)]
 
 
-def compute_expressions(A, B):
-    a = (not A and not B) or A
-    b = B or (not A and not B)
-    c = B and not (A and not B)
-    return a, b, c
+def compute_expressions(a, b):
+    x = (not a and not b) or a
+    y = b or (not a and not b)
+    z = b and not (a and not b)
+    return x, y, z
 
 
-for A, B in num:
-    a, b, c = compute_expressions(A, B)
-    print(f"А={A}, B={B}:")
-    print(f"\ta) не А и не В или А = {a}")
-    print(f"\tb) В или не А и не В = {b}")
-    print(f"\tv) В и не (А и не В) = {c}")
+for a, b in num:
+    x, y, z = compute_expressions(a, b)
+    print(f"a = {a}, b = {b}:")
+    print(f"\ta) not a and not b or a = {x}")
+    print(f"tb) b or not a and not b = {y}")
+    print(f"\tv) b and not (a and not b) = {z}")
     print()
---
+++ task18.py
@@ -0,0 +1,6 @@
+for x in (True, False):
+    for y in (True, False):
+        for z in (True, False):
+            d
+            print(f'not (not {x} or {y} and {z}) or {x} = {x}')
+            print(f'not {y and not x or not y}')
